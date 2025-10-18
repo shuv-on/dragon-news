68cd2db6a87f13c08e6db0bc10851b79206917a9
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2025-10-18 16:45:02 +0600

    Marquee updated

--- dragon-news/src/components/Header/Header.jsx
+++ dragon-news/src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../../assets/logo.png'
 import { format } from 'date-fns';
+import Marquee from 'react-fast-marquee';
 const Header = () => {
     const today = new Date();
 
@@ -12,6 +13,16 @@ const Header = () => {
                 <p className='text-secondary'>Journalism Without Fear or Favour</p>
                 <p className='text-secondary'><span className='font-bold'>{format(today, "EEEE")}</span> {formattedDate}</p>
             </div>
+            <div className=' bg-base-100'>
+                <div className='w-11/12 mx-auto  p-2 flex gap-5'>
+                   <button className='btn bg-base-200 text-white ' >Latest</button>
+                   <Marquee >
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi, a similique consectetur adipisci voluptates dolore, facilis dolores tenetur dignissimos saepe ab harum cumque enim molestiae! Ad incidunt quos omnis inventore.</p>
+                   </Marquee>
+                   
+                </div>
+            </div>
+            
         </div>
     );
 };
