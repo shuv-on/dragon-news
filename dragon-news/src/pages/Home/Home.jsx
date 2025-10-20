51d5ac522a4963c68ed2a3fb4e55eee65c6b6956
Md Shishir Kaysar Shuvon <mdshishirkaysarshuvon.ict.mbstu@gmail.com>
2025-10-20 22:43:54 +0600

    Home section update

--- dragon-news/src/pages/Home/Home.jsx
+++ dragon-news/src/pages/Home/Home.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <p>Hello I am Home</p>
+            <div className='w-11/12 mx-auto grid grid-cols-12 gap-4'>
+                <div className=' col-span-3 *:border-2'>
+                    <p>Shuvon</p>
+                </div>
+                  <div className= 'col-span-6 *:border-2'>
+                    <p>Shuvon</p>
+                </div>
+                  <div className='col-span-3 *:border-2'>
+                    <p>Shuvon</p>
+                </div>
+            </div>
         </div>
     );
 };
